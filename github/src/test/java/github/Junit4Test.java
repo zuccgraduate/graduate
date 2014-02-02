cd0ee79610bf7414b54ce3bc7c0d60c5ef727771
jiangyu <jiangyu@orange>
2014-02-02 11:11:28 +0800

    junit4

---
+++ github/src/test/java/github/Junit4Test.java
@@ -0,0 +1,14 @@
+package github;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Junit4Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
